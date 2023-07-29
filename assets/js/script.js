33d4673099b1f488e8a6555d6583858fcc8b8df2
unknown <blancardt@merrimack.edu>
2023-07-29 13:56:06 -0400

    added the current date to the text field below the header.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //Create a variable to keep track of the day.
+  var today = dayjs();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +22,22 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  //define and populate variables for the date that will be displayed below the header.
+  var numberDay = today.format('D');
+  var dayTense;
+  if (numberDay == 1) {
+    dayTense = "st";
+  }
+  else if (numberDay == 2) {
+    dayTense = "nd";
+  }
+  else if (numberDay == 3) {
+    dayTense = "rd";
+  }
+  else {
+    dayTense = "th";
+  }
+
+  //populate the text field with the current date.
+  $('#currentDay').text(today.format('dddd, MMMM D') + dayTense);
 });
